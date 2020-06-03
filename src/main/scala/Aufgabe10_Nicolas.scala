a55c495de7495ed352d1aa3b97725015f3d0af01
nicolas.tschirr <nicolas.tschirr@adesso.de>
2020-06-03 16:48:12 +0200

    wip

--- src/main/scala/Aufgabe10_Nicolas.scala
+++ src/main/scala/Aufgabe10_Nicolas.scala
@@ -1,10 +1,18 @@
-import java.io.File
+import java.io.{ByteArrayOutputStream, File, PrintWriter}
+import java.nio.file.{Files, Paths}
+
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.apache.spark.{SparkConf, SparkContext}
+import java.io.BufferedWriter
+import java.io.IOException
+import java.nio.charset.Charset
+import java.nio.file.Files
+
 
 object Aufgabe10_Nicolas {
   val AppName:String = "aufgabe10"
-  val Languages:List[String] = List("German")
+  val Languages:List[String] = List("Dutch")
   //val Languages:List[String] = List("Dutch", "English", "French", "German", "Italian", "Russian", "Spanish", "Ukrainian")
   val AnalysisDir:String = "src/main/resources/analysis/"
   val ResultDir:String = "src/main/resources/result/"
@@ -42,7 +50,7 @@ object Aufgabe10_Nicolas {
           .subtractByKey(sc.makeRDD(Array(("",1)))) //remove flatMap => split entry of empty lines
           .sortBy(_._2, ascending = false)
 
-        counts.coalesce(1)
+        counts.repartition(1)
           .saveAsTextFile(ResultDir + lang + "/" + file.getName + "/" + System.currentTimeMillis())
         if (top10 == null) {
           top10 = counts
@@ -53,8 +61,31 @@ object Aufgabe10_Nicolas {
     })
 
     val stopwords = sc.textFile(StopWordsDir + lang + ".txt").map(word => (word.toLowerCase, 1))
-    top10.subtractByKey(stopwords).reduceByKey(_+_).sortBy(_._2, ascending = false)
-      .zipWithIndex().filter(_._2 < 10).coalesce(1).saveAsTextFile(ResultDir + "top10/" + lang + "/" + System.currentTimeMillis())
+    val spark = SparkSession.builder.master("local").getOrCreate;
+    import spark.implicits._ //der Input muss hier bleiben
+    val top10List = top10.subtractByKey(stopwords).reduceByKey(_+_).sortBy(_._2, ascending = false)
+      .zipWithIndex().filter(_._2 < 10).collect()
+    var output : Seq[(String, String, String)] = Seq()
+    top10List.foreach(item => {
+      output = output :+ new Tuple3(item._2 + "", item._1._1, item._1._2 + "")
+    })
+    val table = output.toDF("rank", "word", "frequency")
+    val outCapture = new ByteArrayOutputStream
+    Console.withOut(outCapture) {
+      table.show()
+    }
+    val result = new String(outCapture.toByteArray)
+    val path = Paths.get("src/main/resources/test.txt")
+    Files.createFile(path)
+
+    try {
+      val writer = Files.newBufferedWriter(path, Charset.forName("UTF-8"))
+      try writer.write(result)
+      catch {
+        case ex: IOException =>
+          ex.printStackTrace()
+      } finally if (writer != null) writer.close()
+    }
 
   }
 }
